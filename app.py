a82c1ae3aa3e95ed24df84a5ceed2c900b627c40
Pankaj Radhakisan Dikale <pankajdikale@gmail.com>
2026-06-26 17:46:43 +0530

    Update comment in app.py to include 'pankaj'

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# this code is good
+# this code is good pankaj
 @app.route('/')
 def hello_world():
     return render_template('index.html')
