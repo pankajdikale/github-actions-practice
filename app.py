5d768271536dce7af0b0f56777283b807bbb45ca
Pankaj Radhakisan Dikale <pankajdikale@gmail.com>
2026-09-03 16:31:08 +0530

    Update comment to include full name

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# this code is good pankaj
+# this code is good pankaj dikale
 @app.route('/')
 def hello_world():
     return render_template('index.html')
